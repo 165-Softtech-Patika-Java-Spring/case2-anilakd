ebfdd17c8dcf981c223d0755c1bfd80d37300f93
Anıl Akdemir <36014310+anilakd@users.noreply.github.com>
2022-02-21 15:22:52 +0300

    add comments to StrStreetService

--- src/main/java/com/anilakdemir/case2anilakd/str/service/StrStreetService.java
+++ src/main/java/com/anilakdemir/case2anilakd/str/service/StrStreetService.java
@@ -25,6 +25,12 @@ public class StrStreetService {
     private final NhbNeighborhoodEntityService nhbNeighborhoodEntityService;
 
 
+    /**
+     * if request has valid parameter for neighborhoodId
+     * this function gets the nhbNeighborhood object from nhbNeighborhoodEntityService
+     * and sets it to strStreet object
+     * saves the strStreet and convert the Dto Object
+     */
     public StrStreetDto save(StrStreetSaveRequestDto strStreetSaveRequestDto){
         StrStreet strStreet = StrStreetMapper.INSTANCE.convertToStrStreet(strStreetSaveRequestDto);
         Long neighborhoodId = strStreetSaveRequestDto.getNeighborhoodId();
@@ -39,6 +45,11 @@ public class StrStreetService {
         }
     }
 
+    /**
+     * if the request parameter has valid id
+     * it sets the name field according to request
+     * then saves it, and convert to Dto Object
+     */
     public StrStreetDto updateStrStreetName(StrStreetUpdateNameRequestDto strStreetUpdateNameRequestDto){
         Long strStreetId = strStreetUpdateNameRequestDto.getId();
         StrStreet strStreet = strStreetEntityService.findById(strStreetId);
@@ -48,12 +59,21 @@ public class StrStreetService {
         return strStreetDto;
     }
 
+    /**
+     * it gets all StrStreet objects which has neighborhood's id = neighborhoodId from DAO as a List
+     * and converts it List<Object> to List<ObjectDto>
+     */
     public List<StrStreetDto> findAllByNeighborhoodId(Long neighborhoodId){
         List<StrStreet> strStreetList = strStreetEntityService.findAllByNeighborhoodId(neighborhoodId);
         List<StrStreetDto> strStreetDtoList = StrStreetMapper.INSTANCE.convertToStrStreetDtoList(strStreetList);
         return strStreetDtoList;
     }
 
+
+    /**
+     * this function is to check if there is a neighborhood object whose parameter is neighborhoodId
+     * if the neighborhood exists, it returns true
+     */
     private boolean controlNeighborhoodtIsExist(Long neighborhoodId){
         return nhbNeighborhoodEntityService.existById(neighborhoodId);
     }
